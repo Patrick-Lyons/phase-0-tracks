c8000e7b51719855b75b7e432ff04d80627e594b
Patrick Lyons <MurphyPML@gmail.com>
2016-08-20 18:49:09 -0700

    Fixed 5.6

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,22 +1,126 @@
-#Ask user for name input, use get.chomps
-#Swap First and Last name
-# - Find the two seperate names (First and Last) by 
-# - - transfering all characters of input into an array
-# - - find the index of the space used to seperate the names
-# - - Assign the index numbers from start to the space as First name in a new array
-# - - Assign the index numbers from the space to the last number as the Last name in a new array
-# Create array for vowels
-# Create array for consonants
-# In First and Last arrays, find matches for vowels, cycle to next. Do the same for consonants
-# Print resutls
+=begin
+	
+rescue Exception => e
+	
+end
+Objective:
+1. swapping the first and last name
+2. changing all the vowels to the next vowel and all of the consonants to the next consonant
 
-puts "Welcome Agent. Please provide the name to be turned into an alias:"
-original_name = gets.chomp
-p original_name
+Plan:
+- take name as string
+- turn string into array, each name as an item.
+- create a "original_first_name" array to store the first name
+- create a "original_last_name" array to store the last name
+- letter change
+- join letters
+- store the first and last names into alias_name variable, swapping the position
+- puts alias_name variable
+=end
 
-split_name = original_name.split('')
-p split_name
+######################
 
-split_name.map! {|letter| letter}
-p split_name
 
+# Take name as a string:
+puts "Welcome to the Alias Creator! When finished, enter quit."
+original_name = nil
+name_database = {}
+
+until original_name == "quit"
+ puts "Enter a name to be alias'ed:"
+ original_name = gets.chomp
+if original_name != "quit" 
+ 
+ # Turn string into an array, each name as an item:
+ original_name_array = original_name.split
+ 
+ # Store each name in a new array:
+ original_first_name = [original_name_array[0]]
+ original_last_name = [original_name_array[1]]
+ 
+ # Letter changing, vowels:
+ 
+ # Create reference array and string:
+ vowels_array = ["a", "e", "i", "o", "u"]
+ vowels_string = "aeioua"
+ 
+ # Turn letters into items:
+ original_first_name = original_first_name[0].split('')
+ original_last_name = original_last_name[0].split('')
+ 
+ # Change vowels to next vowel:
+ modified_first_name = original_first_name.map do |letter|
+ 	if vowels_array.find_index(letter) != nil #is the letter a vowel?
+ 		letter = vowels_string[vowels_array.find_index(letter) + 1]
+ 	else
+ 		letter
+ 	end
+ end
+ 
+ modified_last_name = original_last_name.map do |letter|
+ 	if vowels_array.find_index(letter) != nil
+ 		letter = vowels_string[vowels_array.find_index(letter) + 1]
+ 	else
+ 		letter
+ 	end
+ end
+ 
+ #Change consonants to next consonant:
+ modified_first_name.map! do |letter|
+ 	if (vowels_array.find_index(letter) == nil) && (letter != "z") && (letter != "Z") #is the letter not a vowel?
+ 		letter = letter.next
+ 		if vowels_array.find_index(letter) != nil #is the next letter a vowel?
+ 			letter = letter.next
+ 		else
+ 			letter
+ 		end
+ 	elsif letter == "z"
+ 		letter = "b"
+ 	elsif letter == "Z"
+ 		letter = "B"
+ 	else
+ 		letter
+ 	end
+ end
+ 
+ modified_last_name.map! do |letter|
+ 	if (vowels_array.find_index(letter) == nil) && (letter != "z") && (letter != "Z")
+ 		letter = letter.next
+ 		if vowels_array.find_index(letter) != nil
+ 			letter = letter.next
+ 		else
+ 			letter
+ 		end
+ 	elsif letter == "z"
+ 		letter = "b"
+ 	elsif letter == "Z"
+ 		letter = "B"
+ 	else
+ 		letter
+ 	end
+ end
+ 
+ # p original_first_name
+ # p modified_first_name
+ 
+ # p original_last_name
+ # p modified_last_name
+ 
+ modified_first_name = modified_first_name.join
+ modified_last_name = modified_last_name.join
+ 
+ #p modified_last_name.class
+ #p modified_first_name.class
+ 
+ alias_name = modified_last_name + " " + modified_first_name
+ puts alias_name
+
+else
+	name_database.each do |original, changed|
+		puts "#{original} is also known as #{changed}."
+	end
+	puts "Good bye."
+end
+ name_database[original_name] = alias_name
+ 
+end
