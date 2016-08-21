3b51204a78c3206df03456af9f1f9d8150022e6f
Patrick Lyons <MurphyPML@gmail.com>
2016-08-20 20:02:28 -0700

    6.1, Puppy .rb files.

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+=begin
+SPECIES ------------------------
+Fluffius Puppius
+
+CHARACTERISTICS ----------------
+Fur: fluffy
+Friendly: yes
+Size: fun
+Loud: varies
+Energy: high
+
+BEHAVIOR -----------------------
+Jump
+Run
+Trip
+Roll
+=end
